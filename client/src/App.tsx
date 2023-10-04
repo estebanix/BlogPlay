1f1bce9340edfb6d8abad6a5a8690feb6185d1ed
estebanix <dakisunshine@gmail.com>
2023-10-04 17:58:58 +0200

    basic HOW structure and style

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -15,13 +15,19 @@ function App() {
       <div className="post">
         <div className="image">
           <img
-            src="https://cdn.britannica.com/16/234216-050-C66F8665/beagle-hound-dog.jpg?w=400&h=300&c=crop"
+            src="https://stimg.cardekho.com/images/carexteriorimages/630x420/Tesla/Model-X/5253/1611841733029/front-left-side-47.jpg?tr=w-456"
             alt="dog"
           />
         </div>
         <div className="texts">
           <h2>Nice Dog</h2>
-          <p>
+          <p className="info">
+            <a href="#" className="author">
+              Samo
+            </a>
+            <time>2023-10-04</time>
+          </p>
+          <p className="summary">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
             delectus qui iusto voluptate nesciunt explicabo accusantium commodi
             ducimus quidem laudantium doloremque mollitia, eum cumque porro,
@@ -32,13 +38,19 @@ function App() {
       <div className="post">
         <div className="image">
           <img
-            src="https://cdn.britannica.com/16/234216-050-C66F8665/beagle-hound-dog.jpg?w=400&h=300&c=crop"
+            src="https://stimg.cardekho.com/images/carexteriorimages/630x420/Tesla/Model-X/5253/1611841733029/front-left-side-47.jpg?tr=w-456"
             alt="dog"
           />
         </div>
         <div className="texts">
           <h2>Nice Dog</h2>
-          <p>
+          <p className="info">
+            <a href="#" className="author">
+              Samo
+            </a>
+            <time>2023-10-04</time>
+          </p>
+          <p className="summary">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
             delectus qui iusto voluptate nesciunt explicabo accusantium commodi
             ducimus quidem laudantium doloremque mollitia, eum cumque porro,
@@ -49,13 +61,19 @@ function App() {
       <div className="post">
         <div className="image">
           <img
-            src="https://cdn.britannica.com/16/234216-050-C66F8665/beagle-hound-dog.jpg?w=400&h=300&c=crop"
+            src="https://stimg.cardekho.com/images/carexteriorimages/630x420/Tesla/Model-X/5253/1611841733029/front-left-side-47.jpg?tr=w-456"
             alt="dog"
           />
         </div>
         <div className="texts">
           <h2>Nice Dog</h2>
-          <p>
+          <p className="info">
+            <a href="#" className="author">
+              Samo
+            </a>
+            <time>2023-10-04</time>
+          </p>
+          <p className="summary">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
             delectus qui iusto voluptate nesciunt explicabo accusantium commodi
             ducimus quidem laudantium doloremque mollitia, eum cumque porro,
