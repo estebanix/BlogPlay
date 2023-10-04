c0072b48e115e5d36f8c1a3d77840415afd57cb6
estebanix <dakisunshine@gmail.com>
2023-10-04 17:40:40 +0200

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,70 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="#" className="logo">
+          MyBlog
+        </a>
+        <nav>
+          <a href="#">Login</a>
+          <a href="#">Register</a>
+        </nav>
+      </header>
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://cdn.britannica.com/16/234216-050-C66F8665/beagle-hound-dog.jpg?w=400&h=300&c=crop"
+            alt="dog"
+          />
+        </div>
+        <div className="texts">
+          <h2>Nice Dog</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+            delectus qui iusto voluptate nesciunt explicabo accusantium commodi
+            ducimus quidem laudantium doloremque mollitia, eum cumque porro,
+            velit voluptatem itaque! Atque, iusto.
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://cdn.britannica.com/16/234216-050-C66F8665/beagle-hound-dog.jpg?w=400&h=300&c=crop"
+            alt="dog"
+          />
+        </div>
+        <div className="texts">
+          <h2>Nice Dog</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+            delectus qui iusto voluptate nesciunt explicabo accusantium commodi
+            ducimus quidem laudantium doloremque mollitia, eum cumque porro,
+            velit voluptatem itaque! Atque, iusto.
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://cdn.britannica.com/16/234216-050-C66F8665/beagle-hound-dog.jpg?w=400&h=300&c=crop"
+            alt="dog"
+          />
+        </div>
+        <div className="texts">
+          <h2>Nice Dog</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+            delectus qui iusto voluptate nesciunt explicabo accusantium commodi
+            ducimus quidem laudantium doloremque mollitia, eum cumque porro,
+            velit voluptatem itaque! Atque, iusto.
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
